b319ac9f5c2d26bf8d50db7ac21368082e2ce569
imnvelvin <121167831+imnvelvin@users.noreply.github.com>
2023-04-03 13:54:14 +0400

    Add files via upload

---
+++ HomeTask1/task1/main.js
@@ -0,0 +1,59 @@
+/* let numbers = [8,2,3,4,5,6]
+
+function yoxla(numbers){
+    var birincinum=numbers[0]
+    if(Array.isArray(numbers)){
+        
+
+        for( let index = 1; index < numbers.length; index++){
+          if(birincinum > numbers[index]){
+              birincinum = numbers [index]
+      
+          }
+          
+      
+        }
+        
+      }
+
+      else('Daxil etdiyiniz bir Array deyil ')
+     
+      return birincinum;
+    
+}
+
+console.log(yoxla(numbers))
+*/
+
+let soz = 'salam'
+function eyniherf(){
+    var birinciherf=[0]
+    var tezearray=[]
+    if(typeof(soz)==='string'){
+        for (let index = 1; index < birinciherf.length; index++) {
+          
+            for (let index2 = index  < index2.length; index--) {
+               if(soz[index==soz[index2]]){
+                
+               }
+                
+            }
+        }
+        return birinciherf
+    }
+    
+
+    
+}
+
+
+console.log(eyniherf(soz))
+
+
+
+
+
+
+
+
+
